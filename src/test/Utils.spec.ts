6c77928ede7183afda6f9669e03f46063597b4b5
Eduardo Ramos <eduardorxs@gmail.com>
2026-03-08 19:30:52 -0300

    Matchers

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
 
@@ -13,6 +13,24 @@ describe("Utils test suite", () => {
 
         // assert:
         expect(actual).toBe(expected);
+    })
+
+
+    it("Should return info for valid string", () => {
+        const actual = getStringInfo("My String")
+
+        expect(actual.lowerCase).toBe('my string')
+        expect(actual.extraInfo).toEqual({}) 
+
+        // expect(actual.characters.length).toBe(9)
+        expect(actual.characters).toHaveLength(9)
+        expect(actual.characters).toContain<string>('M')
+        
+        // expect(actual.extraInfo).not.toBe(undefined)
+        // expect(actual.extraInfo).not.toBeUndefined()
+        // expect(actual.extraInfo).toBeDefined()
+        expect(actual.extraInfo).toBeTruthy()
+
     })
 
 });
