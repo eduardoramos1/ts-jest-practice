d265f3aea1caa8f086259555a17cd4af53657726
Eduardo Ramos <eduardorxs@gmail.com>
2026-03-10 20:49:22 -0300

    Good practice with describes

--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -3,6 +3,7 @@ import { getStringInfo, toUpperCase } from "../app/Utils";
 describe("Utils test suite", () => {
 
     it("Should return uppercase of valid string", () => {
+        // Padrão de teste AAA
         // Convenção comum de estruturação de um teste: arrange, act, assert
         // arrange:
         const sut = toUpperCase // sut = system under test
@@ -15,22 +16,39 @@ describe("Utils test suite", () => {
         expect(actual).toBe(expected);
     })
 
+    describe('getStringInfo for arg My-string should', () => {
+        it('should return right length', () => {
+            const actual = getStringInfo("My String")
+            expect(actual.characters).toHaveLength(9)
+        })
 
-    it("Should return info for valid string", () => {
-        const actual = getStringInfo("My String")
+        it('should return right lowercase', () => {
+            const actual = getStringInfo("My String")
+            expect(actual.lowerCase).toBe('my string')
+        })
 
-        expect(actual.lowerCase).toBe('my string')
-        expect(actual.extraInfo).toEqual({}) 
-
-        // expect(actual.characters.length).toBe(9)
-        expect(actual.characters).toHaveLength(9)
-        expect(actual.characters).toContain<string>('M')
-        
-        // expect(actual.extraInfo).not.toBe(undefined)
-        // expect(actual.extraInfo).not.toBeUndefined()
-        // expect(actual.extraInfo).toBeDefined()
-        expect(actual.extraInfo).toBeTruthy()
-
+        it('should return right uppercase', () => {
+            const actual = getStringInfo("My String")
+            expect(actual.upperCase).toBe('MY STRING')
+        })
     })
+
+
+    // it("Should return info for valid string", () => {
+    //     const actual = getStringInfo("My String")
+
+    //     expect(actual.lowerCase).toBe('my string')
+    //     expect(actual.extraInfo).toEqual({}) 
+
+    //     // expect(actual.characters.length).toBe(9)
+    //     expect(actual.characters).toHaveLength(9)
+    //     expect(actual.characters).toContain<string>('M')
+        
+    //     // expect(actual.extraInfo).not.toBe(undefined)
+    //     // expect(actual.extraInfo).not.toBeUndefined()
+    //     // expect(actual.extraInfo).toBeDefined()
+    //     expect(actual.extraInfo).toBeTruthy()
+
+    // })
 
 });
